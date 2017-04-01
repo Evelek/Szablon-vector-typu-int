8e4e21a4e726fa657b2039767cac351b7101a07b
Evelek <majkel196@onet.pl>
2017-04-01 16:18:09 +0200

    Update vector.c
    Poprawa algorytmu przy wykorzystaniu nowej zmiennej struktury vector 'size_t rozmiar'.

--- vector.c
+++ vector.c
@@ -2,33 +2,60 @@
 #include <stdlib.h>
 #include "vector.h"
 
-static int vector_rozmiar = 1;
-
 void alokuj_vector(struct vector *wsk) {
 	wsk->tab = malloc(sizeof(int));
-	vector_rozmiar++;
+	wsk->rozmiar = 0;
 }
 
 void dodaj_element(struct vector *wsk, const int wartosc) {
-	wsk->tab = realloc(wsk->tab, vector_rozmiar * sizeof(int));
-	vector_rozmiar++;
-
-	wsk->tab[vector_rozmiar - 3] = wartosc;
+	wsk->tab = realloc(wsk->tab, (wsk->rozmiar + 1) * sizeof(int));
+	wsk->tab[wsk->rozmiar] = wartosc;
+	wsk->rozmiar++;
 }
 
 void usun_element(struct vector *wsk) {
-	wsk->tab[vector_rozmiar - 3] = 0;
-	free(wsk->tab[vector_rozmiar - 3]);
-	vector_rozmiar--;
+	wsk->tab[wsk->rozmiar-1] = 0;
+	free(wsk->tab[(wsk->rozmiar)-1]);
+	//free(wsk->tab[vector_rozmiar - 3]);
+	wsk->rozmiar--;
 }
 
 void wyswietl_vector(const struct vector *wsk) {
-	for (int i = 0; i < vector_rozmiar - 2; ++i)
+	for (int i = 0; i < wsk->rozmiar; ++i)
 		printf("%d ", wsk->tab[i]);
 }
 
 size_t zwroc_rozmiar(const struct vector *wsk) {
-	return vector_rozmiar - 2;
+	return wsk->rozmiar;
+}
+
+void wyczysc_vector(struct vector *wsk) {
+	free(wsk->tab);
+	wsk->rozmiar = 0;
+}
+
+void sortuj(struct vector *wsk) {
+	int lewy = 0;
+	int prawy = wsk->rozmiar-1;
+	if (prawy <= lewy) return;
+	int i = lewy - 1, j = prawy + 1,
+		pivot = wsk->tab[(lewy + prawy) / 2];
+	while (1)
+	{
+		while (pivot>wsk->tab[++i]);
+		while (pivot<wsk->tab[--j]);
+		if (i <= j) {
+			int temp = wsk->tab[i];
+			wsk->tab[i] = wsk->tab[j];
+			wsk->tab[j] = temp;
+		}
+		else
+			break;
+	}
+	if (j > lewy)
+		sortuj_2(wsk, lewy, j);
+	if (i < prawy)
+		sortuj_2(wsk, i, prawy);
 }
 
 void sortuj_2(struct vector *wsk, int lewy, int prawy) {
@@ -51,31 +78,4 @@ void sortuj_2(struct vector *wsk, int lewy, int prawy) {
 		sortuj_2(wsk, lewy, j);
 	if (i < prawy)
 		sortuj_2(wsk, i, prawy);
-}
-void sortuj(struct vector *wsk) {
-	int lewy = 0;
-	int prawy = vector_rozmiar - 3;
-	if (prawy <= lewy) return;
-	int i = lewy - 1, j = prawy + 1,
-		pivot = wsk->tab[(lewy + prawy) / 2];
-	while (1)
-	{
-		while (pivot>wsk->tab[++i]);
-		while (pivot<wsk->tab[--j]);
-		if (i <= j) {
-			int temp = wsk->tab[i];
-			wsk->tab[i] = wsk->tab[j];
-			wsk->tab[j] = temp;
-		}
-		else
-			break;
-	}
-	if (j > lewy)
-		sortuj_2(wsk, lewy, j);
-	if (i < prawy)
-		sortuj_2(wsk, i, prawy);
-}
-
-void wyczysc_vector(struct vector *wsk) {
-	free(wsk->tab);
 }
