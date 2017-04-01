921a8e71410c9e50634ff67c024dc38133dfa6e3
Evelek <majkel196@onet.pl>
2017-04-01 17:07:32 +0200

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,18 +1,19 @@
 #ifndef VECTOR_H_
 #define VECTOR_H_
+#include <stdbool.h>
 
 typedef struct vector {
 	int *tab;
 	size_t rozmiar;
 } Vector;
 
-//alokuje pamiec dla jednego elementu
+//funkcja alokuje pamiec dla jednego elementu
 void alokuj_vector(struct vector *wsk);
 
-//alokuje pamiec dla nastepnego elementu i dodaje nowy element na koniec vectora
+//funkcja alokuje pamiec dla nastepnego elementu i dodaje nowy element na koniec vectora
 void dodaj_element(struct vector *wsk, const int wartosc);
 
-//czysci pamiec ostatniego elementu vectora i usuwa ostatni element vectora
+//funkcja czysci pamiec ostatniego elementu vectora i usuwa ostatni element vectora
 void usun_element(struct vector *wsk);
 
 //wyswietla wszystkie elementy vectora
@@ -21,11 +22,11 @@ void wyswietl_vector(const struct vector *wsk);
 //zwraca rozmiar vectora
 size_t zwroc_rozmiar(const struct vector *wsk);
 
-//czysci pamiec vectora, vector jest usuwany
+//funkcja czysci pamiec vectora, vector jest usuwany
 void wyczysc_vector(struct vector *wsk);
 
-//sortuje vector
-void sortuj_2(struct vector *wsk, int lewy, int prawy);
-void sortuj(struct vector *wsk);
+//sortuje vector algorytmem QuickSort
+void sortuj(struct vector *wsk); //wywoluje funkcje sortuj_algorytm
+void sortuj_algorytm(struct vector *wsk, int lewy, int prawy); //funkcja rekurencyjna
 
 #endif // !VECTOR_H_
